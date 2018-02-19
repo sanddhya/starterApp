b3c21a61b1254204ae423a276fd699888cabd250
NEX61AN <arpit.gupta@pb.com>
2018-02-19 18:10:25 +0530

    new

--- app/modules/prospects/controllers/prospects.controller.js
+++ app/modules/prospects/controllers/prospects.controller.js
@@ -3,7 +3,7 @@
   'use strict';
 
   angular.module('pb.ds.prospects').controller('ProspectsController',
-    function ($log, $uibModal, $http, $scope) {
+    function ($log, $uibModal, $http, $scope, $interval) {
 
       var _this = this;
 
@@ -12,16 +12,22 @@
       this.byCarrierAllowed = true;
       this.displayNavigator = this.byLocationAllowed && this.byStatusAllowed && this.byCarrierAllowed;
       _this.barGraph = false;
-
+    
       _this.colors = ['#3e53a4', '#cf0989', '#009bdf', '#ef8200', '#edb700', '#a03f9b', '#00b140', '#0072b8'];
       _this.mono = ['#00436E', '#005A93', '#0072B8', '#66AAD4', '#CCE3F1', '#E5F1F8'];
 
-      _this.numberOfOpportunities = 1123;
+      $scope.numberOfOpportunities  = 6019;
       _this.existingOpportunityBarGraphLabels = [];
       _this.existingOpportunityBarGraphCount = [];
 
+      $scope.$watch('numberOfOpportunities', function (newValue, oldValue) {
+        console.log(newValue);
+        console.log(oldValue);
+        $scope.numberOfOpportunities  = newValue;
+      });
+    
       // ExistingOpportunityBarGraph
-      _this.getExistingOpportunityBarGraph = function () {
+      _this.getExistingOpportunityBarGraph = function() {
         _this.existingOpportunityBarGraph = {
           labels: _this.existingOpportunityBarGraphLabels,
           options: {
@@ -30,7 +36,7 @@
               yAxes: [{
                 scaleLabel: {
                   display: true,
-                  labelString: 'Number of clients (' + _this.numberOfOpportunities + ')'
+                  labelString: 'Number of clients (' +  $scope.numberOfOpportunities  + ')'
                 }
               }],
               xAxes: [{
@@ -48,33 +54,28 @@
           click: function (points, evt) {
             //
           }
-        };
-      };
-      _this.dataName = [];
-
-      _this.getExistingOpportunityBarGraphData = function () {
-        $http.get("../modules/prospects/sampleCSVData.csv").then(function (response) {
+        };        
+      }; 
+      _this.dataName = []; 
+      _this.getExistingOpportunityBarGraphData = function() {
+        $http.get("../modules/prospects/sampleCSVData.csv").then(function(response) {
           console.log(response);
           Papa.parse(response.data, {
             worker: true,
             delimiter: ',',
             header: true,
-            step: function (results) {
+            step: function(results) {
               _this.dataName.push(results.data[0]);
             },
-            complete: function () {
+            complete: function() {
               console.log(_this.dataName);
-              for (var i = 0; i < 10; i++) {
+              for(var i = 0;i < 10; i++) {
                 _this.existingOpportunityBarGraphLabels.push(_this.dataName[i].Zip_Code);
                 _this.existingOpportunityBarGraphCount.push(_this.dataName[i].Median_Age);
-              }
-              ;
+              };
               console.log(_this.existingOpportunityBarGraphLabels);
               console.log(_this.existingOpportunityBarGraphCount);
-              var res = alasql('SELECT dep, SUM(qt) AS qt, SUM(qt*price) AS amt, AGGR(amt/qt) AS price FROM ? GROUP BY dep', [_this.dataName]);
-              console.log('*****************');
-              console.log(res);
-
+              var res = alasql('SELECT dep, SUM(qt) AS qt, SUM(qt*price) AS amt, AGGR(amt/qt) AS price FROM ? GROUP BY dep',[_this.dataName]);
               _this.getExistingOpportunityBarGraph();
             }
           });
@@ -85,31 +86,34 @@
       // ExistingClientsBarGraph
       _this.existingClientsFilter = [
         {
-          'name': 'Label_Printer',
+          'value': 'label_printers',
+          'name': 'Label Printers',
           'isSelected': false
         },
         {
-          'name': 'Multicarrier_Subscription',
+          'value': 'multicarrier_subscriptions',
+          'name': 'Multicarrier Subscriptions',
           'isSelected': false
         },
         {
-          'name': 'SIC8_New',
+          'value': 'SIC8_new',
+          'name': 'SIC8',
           'isSelected': false
         }
       ]
       _this.existingClientsBarGraphLabels = [];
       _this.existingClientsBarGraphCount = [];
-      _this.getExistingClientsBarGraph = function () {
+      _this.getExistingClientsBarGraph = function() {
         _this.barGraph = true;
         _this.existingClientsBarGraph = {
-          labels: _this.existingClientsBarGraphLabels,
+          labels:  _this.existingClientsBarGraphLabels,
           options: {
             scaleShowGridLines: false,
             scales: {
               yAxes: [{
                 scaleLabel: {
                   display: true,
-                  labelString: 'Number of clients (' + _this.numberOfOpportunities + ')'
+                  labelString: 'Number of clients (' +  $scope.numberOfOpportunities  + ')'
                 }
               }],
               xAxes: [{
@@ -128,41 +132,77 @@
             //
           }
         };
-        $scope.$on('chart-create', function (evt, chart) {
-          console.log(chart);
-        });
       };
-
-      _this.getExistingClientsBarGraphData = function () {
-        $http.get("../modules/prospects/existingClients.csv").then(function (response) {
+      _this.filter = {
+        'label': false,
+        'multicarrierSubscription': false
+      };
+      _this.dataName2 = [];
+      _this.getExistingClientsBarGraphData = function() {
+        _this.dataName2 = [];
+        $http.get("../modules/prospects/existingClients.csv").then(function(response) {
           console.log(response);
           Papa.parse(response.data, {
             worker: true,
             delimiter: ',',
             header: true,
-            step: function (results) {
-              _this.dataName.push(results.data[0]);
+            step: function(results) {
+              _this.dataName2.push(results.data[0]);
             },
-            complete: function () {
-              console.log(_this.dataName);
-              alasql('SELECT score, ship_to_account_number FROM ? WHERE has_purchasepower = "Yes" ', [_this.dataName], function (res) {
-                console.log(res);
-                for (var i = 0; i < 10; i++) {
-                  _this.existingClientsBarGraphLabels.push(res[i].ship_to_account_number);
-                  _this.existingClientsBarGraphCount.push(parseInt(res[i].score));
-                }
-                ;
-                console.log(_this.existingClientsBarGraphLabels);
-                console.log(_this.existingClientsBarGraphCount);
-                _this.getExistingClientsBarGraph();
-              });
+            complete: function() {
+              // var param = [];
+              var labelCount = 0;
+              console.log(labelCount);
+              console.log(_this.dataName2);
+              for (let i = 0; i<_this.dataName2.length; i++) {
+                if (_this.filter.label === true && _this.filter.multicarrierSubscription === true && _this.dataName2[i].score > 20) {
+                  if (_this.dataName2[i].label_printers === "yes" && _this.dataName2[i].multicarrier_subscriptions === "yes") {
+                    labelCount++;
+                  }
+                } else if (_this.filter.label === true && _this.filter.multicarrierSubscription === false && _this.dataName2[i].score > 20) {
+                    if (_this.dataName2[i].label_printers === "yes") {
+                      labelCount++;
+                    }
+                } else if (_this.filter.label === false && _this.filter.multicarrierSubscription === true && _this.dataName2[i].score > 20) {
+                    if (_this.dataName2[i].multicarrier_subscriptions === "yes") {
+                      labelCount++;
+                    }
+                } else if(_this.dataName2[i].score > 20) {
+                      labelCount++
+                } 
+              }
+              console.log(labelCount);
+              $scope.numberOfOpportunities  = labelCount;
+              _this.count($scope.numberOfOpportunities);
+              // var res = 'SELECT label_printers FROM ? WHERE label_printers = "Yes"'
+              // alasql(res,[_this.dataName2],function(res){
+              //   console.log(res);
+              //   for(var i = 0; i < 10; i++) {
+              //     _this.existingClientsBarGraphLabels.push(res[i].bpn);
+              //     _this.existingClientsBarGraphCount.push(parseInt(res[i].score));
+              //   };
+              //   console.log(_this.existingClientsBarGraphLabels);
+              //   console.log(_this.existingClientsBarGraphCount);
+              //   _this.getExistingClientsBarGraph();
+              // });
             }
           });
         });
       };
 
+      _this.count = function(count) {
+        $scope.numberOfOpportunities = count;
+      };  
+
+      $scope.callAtInterval = function() {
+        $scope.numberOfOpportunities = $scope.numberOfOpportunities;
+      }
+
+      $interval( function(){ $scope.callAtInterval(); }, 500);
+
+      
       // ShippingOpportunitiesBarGraph
-      _this.getShippingOpportunitiesBarGraph = function () {
+      _this.getShippingOpportunitiesBarGraph = function() {
         _this.shippingOpportunitiesBarGraph = {
           labels: ['2011', '2012', '2013', '2014', '2015'],
           options: {
@@ -171,7 +211,7 @@
               yAxes: [{
                 scaleLabel: {
                   display: true,
-                  labelString: 'Number of clients (' + _this.numberOfOpportunities + ')'
+                  labelString: 'Number of clients (' +  $scope.numberOfOpportunities  + ')'
                 }
               }],
               xAxes: [{
@@ -190,26 +230,26 @@
             //
           }
         };
-      };
-
-      _this.getShippingOpportunitiesBarGraphData = function () {
+      };      
+      
+      _this.getShippingOpportunitiesBarGraphData = function() {
         // $http.get("welcome.htm").then(function(response) {
         //   response.data.forEach((data) => {
         //     _this.labels.push();
         //     _this.count.push();
         //   });
-        // _this.totalEmailsSent = 2000;
-        // _this.totalEmailsSent = 1000;
-        // _this.totalEmailsSent = 500;
+            // _this.totalEmailsSent = 2000;
+            // _this.totalEmailsSent = 1000;
+            // _this.totalEmailsSent = 500;
         //   _this.getShippingOpportunitiesBarGraph();
         // });
         _this.getShippingOpportunitiesBarGraph();
-      };
-
+      }; 
+      
       _this.getShippingOpportunitiesBarGraphData();
       _this.getExistingOpportunityBarGraphData();
       _this.getExistingClientsBarGraphData();
-
+      _this.filter;
       _this.open = function (item) {
         $uibModal.open({
           templateUrl: 'modules/prospects/templates/modal.html',
@@ -217,10 +257,16 @@
           size: 'md',
           resolve: {
             itemResolve: function () {
-              return item;
+              return _this.filter;
             }
           }
-        });
+        }).result.then(function(response){
+            console.log(response);
+            _this.filter = response;
+            _this.getExistingClientsBarGraphData();
+         }, function(){
+            //
+         });
       };
 
     });
